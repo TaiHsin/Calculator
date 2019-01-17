f27f118eda9fcc00cdf159cd7cb01fcb3ee55255
taihsinlee <taihsinlee@kkbox.com>
2019-01-17 18:54:54 +0800

    Implemented input number function

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -10,23 +10,67 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *inputTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultTextField;
+@property (weak, nonatomic) IBOutlet UILabel *symbolsLabel;
+
+//@property NSString *inputString;
+
+//@property NSMutableString *inputString;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.resultTextField.text = @" ";
+    self.inputTextField.text = @"0";
+    
 }
 
-- (IBAction)numberInput:(UIButton *)sender {
+- (IBAction)inputNumber:(UIButton *)sender {
+    
+    NSString *inputString = [[sender titleLabel] text];
+    NSString *currentString = self.inputTextField.text;
+    
+    
+    if ([currentString length] == 1 && [[currentString substringFromIndex: 0] isEqualToString: @"0"]) {
+        
+        currentString = @"";
+    }
+    
+    self.inputTextField.text = [currentString stringByAppendingString: inputString];
 }
 
-
-- (IBAction)allClear:(id)sender {
+- (IBAction)mathSymbolExpression:(UIButton *)sender {
+    
+    NSString *inputString = [[sender titleLabel] text];
+    NSString *currentString = self.inputTextField.text;
+    NSString *resultString = self.resultTextField.text;
+    NSDecimalNumber *currentNumber = [NSDecimalNumber decimalNumberWithString: currentString];
+//    NSDecimalNumber *resultNumber = [[NSDecimalNumber alloc] init];
+    NSDecimalNumber *resultNumber = [NSDecimalNumber numberWithInt: 0];
+    
+    if (![resultString isEqualToString: @" "]) {
+        
+        resultNumber = [NSDecimalNumber decimalNumberWithString: resultString];
+    }
+    
+    if ([inputString isEqualToString: @"+"]) {
+        
+        [resultNumber decimalNumberByAdding: currentNumber];
+        resultString = [NSString stringWithFormat: @"%@", resultNumber];
+        self.resultTextField.text = resultString;
+    }
 }
 
-- (IBAction)memoryClear:(UIButton *)sender {
+- (IBAction)clearAll:(UIButton *)sender {
+
+    self.inputTextField.text = @"0";
+}
+
+- (IBAction)clearMemory:(UIButton *)sender {
 }
 
 
