b25984c8c90943ce6ef24d57a539a10c7049855d
TaiHsin <peterlee0466@gmail.com>
2019-01-18 00:23:00 +0800

    Number calculate function

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -10,64 +10,128 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet UILabel *inputTextField;
-@property (weak, nonatomic) IBOutlet UILabel *resultTextField;
-@property (weak, nonatomic) IBOutlet UILabel *symbolsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *inputLabel;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (weak, nonatomic) IBOutlet UILabel *operatorLabel;
+//@property NSString *operator;
 
-//@property NSString *inputString;
-
-//@property NSMutableString *inputString;
 @end
 
 @implementation ViewController
 
+- (NSString *)calculateResult:(NSString *)operator
+                   withResult:(NSDecimalNumber *)resultNumber
+                  withCurrent:(NSDecimalNumber *)currentNumber {
+    NSString *result = [[NSString alloc] init];
+    
+    if ([operator isEqualToString: @"+"]) {
+        resultNumber = [resultNumber decimalNumberByAdding: currentNumber];
+        
+    } else if ([operator isEqualToString: @"-"]) {
+        resultNumber = [resultNumber decimalNumberBySubtracting: currentNumber];
+    
+    } else if ([operator isEqualToString: @"*"]) {
+        resultNumber = [resultNumber decimalNumberByMultiplyingBy: currentNumber];
+    
+    } else if ([operator isEqualToString: @"/"]) {
+        resultNumber = [resultNumber decimalNumberByDividingBy: currentNumber];
+        
+    } else if ([operator isEqualToString: @"="]) {
+        resultNumber = resultNumber;
+        
+    } else {
+        resultNumber = currentNumber;
+    }
+    
+    result = [NSString stringWithFormat: @"%@", resultNumber];
+    return result;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.resultTextField.text = @" ";
-    self.inputTextField.text = @"0";
+    self.resultLabel.text = @" ";
+    self.inputLabel.text = @"0";
+    self.operatorLabel.text = @" ";
     
 }
 
 - (IBAction)inputNumber:(UIButton *)sender {
-    
     NSString *inputString = [[sender titleLabel] text];
-    NSString *currentString = self.inputTextField.text;
-    
+    NSString *currentString = self.inputLabel.text;
     
     if ([currentString length] == 1 && [[currentString substringFromIndex: 0] isEqualToString: @"0"]) {
-        
         currentString = @"";
     }
     
-    self.inputTextField.text = [currentString stringByAppendingString: inputString];
+    self.inputLabel.text = [currentString stringByAppendingString: inputString];
 }
 
 - (IBAction)mathSymbolExpression:(UIButton *)sender {
     
     NSString *inputString = [[sender titleLabel] text];
-    NSString *currentString = self.inputTextField.text;
-    NSString *resultString = self.resultTextField.text;
+    NSString *currentString = self.inputLabel.text;
+    NSString *resultString = self.resultLabel.text;
+    NSString *operator = self.operatorLabel.text;
     NSDecimalNumber *currentNumber = [NSDecimalNumber decimalNumberWithString: currentString];
-//    NSDecimalNumber *resultNumber = [[NSDecimalNumber alloc] init];
-    NSDecimalNumber *resultNumber = [NSDecimalNumber numberWithInt: 0];
+    NSDecimalNumber *resultNumber = (NSDecimalNumber *)[NSDecimalNumber numberWithInt: 0];
     
     if (![resultString isEqualToString: @" "]) {
-        
         resultNumber = [NSDecimalNumber decimalNumberWithString: resultString];
     }
-    
+
     if ([inputString isEqualToString: @"+"]) {
-        
-        [resultNumber decimalNumberByAdding: currentNumber];
-        resultString = [NSString stringWithFormat: @"%@", resultNumber];
-        self.resultTextField.text = resultString;
+        resultString = [self calculateResult:operator
+                                  withResult:resultNumber
+                                 withCurrent:currentNumber];
+
+        self.resultLabel.text = resultString;
+        self.operatorLabel.text = inputString;
+        self.inputLabel.text = @"0";
+
+    } else if ([inputString isEqualToString: @"-"]) {
+        resultString = [self calculateResult:operator
+                                  withResult:resultNumber
+                                 withCurrent:currentNumber];
+
+        self.resultLabel.text = resultString;
+        self.operatorLabel.text = inputString;
+        self.inputLabel.text = @"0";
+
+    } else if ([inputString isEqualToString: @"*"]) {
+        resultString = [self calculateResult:operator
+                                  withResult:resultNumber
+                                 withCurrent:currentNumber];
+
+        self.resultLabel.text = resultString;
+        self.operatorLabel.text = inputString;
+        self.inputLabel.text = @"0";
+
+    } else if ([inputString isEqualToString: @"/"]) {
+
+        resultString = [self calculateResult:operator
+                                  withResult:resultNumber
+                                 withCurrent:currentNumber];
+
+        self.resultLabel.text = resultString;
+        self.operatorLabel.text = inputString;
+        self.inputLabel.text = @"0";
+
+    } else {
+        resultString = [self calculateResult:operator
+                                  withResult:resultNumber
+                                 withCurrent:currentNumber];
+
+        self.resultLabel.text = resultString;
+        self.operatorLabel.text = inputString;
+        self.inputLabel.text = @"0";
     }
 }
 
 - (IBAction)clearAll:(UIButton *)sender {
-
-    self.inputTextField.text = @"0";
+    self.inputLabel.text = @"0";
+    self.resultLabel.text = @" ";
+    self.operatorLabel.text = @" ";
 }
 
 - (IBAction)clearMemory:(UIButton *)sender {
